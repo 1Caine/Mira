711ddf10c78883239713a07dfd4f488b130489ba
caine1 <166988079+1Caine@users.noreply.github.com>
2025-07-17 15:41:14 +0200

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        mira: '#00FFB3',
+        dark: '#0D0D0D',
+      },
+    },
+  },
+  plugins: [],
+}
