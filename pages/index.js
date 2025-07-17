397fe18e4e29787e2514a32b8e6dc5a5c4827c1e
caine1 <166988079+1Caine@users.noreply.github.com>
2025-07-17 16:27:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-dark text-mira text-center min-h-screen flex flex-col items-center justify-center">
+    <div className="bg-dark text-mira min-h-screen flex items-center justify-center flex-col">
       <h1 className="text-4xl font-bold">Meet Mira</h1>
       <p>Your crypto genius assistant</p>
     </div>
